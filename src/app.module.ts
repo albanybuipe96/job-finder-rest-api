f60d3b003d95ffe1073a3185e9f921ae0a770646
albanybuipe96 <mohammedarahman96@gmail.com>
2024-02-11 02:37:06 +0000

    added .env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,51 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { MiddlewareConsumer, Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/entities/user.entity'
+const cookieSession = require('cookie-session')
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { COOKIE_SESSION_SECRET, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_TYPE, DB_USERNAME } from './constants/config.constants'
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>(DB_HOST),
+          port: config.get<number>(DB_PORT),
+          username: config.get<string>(DB_USERNAME),
+          password: config.get<string>(DB_PASSWORD),
+          database: config.get<string>(DB_NAME),
+          entities: [User],
+          synchronize: true,
+          logging: true,
+          autoLoadEntities: true,
+        }
+      }
+    }),
+    JobsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cookieSession({
+        keys: [this.configService.get(COOKIE_SESSION_SECRET)]
+      }))
+      .forRoutes('*')
+  }
+}
---
+++ src/constants/config.constants.ts
@@ -0,0 +1,7 @@
+export const COOKIE_SESSION_SECRET = 'COOKIE_SESSION_SECRET'
+export const DB_HOST = 'DB_HOST'
+export const DB_TYPE = 'DB_TYPE'
+export const DB_PORT = 'DB_PORT'
+export const DB_PASSWORD = 'DB_PASSWORD'
+export const DB_USERNAME = 'DB_USERNAME'
+export const DB_NAME = 'DB_NAME'
